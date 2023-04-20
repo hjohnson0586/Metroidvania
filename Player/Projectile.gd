e1e1439c6e3001c69ee9f3b9f8d70903f9f38810
Harry Johnson <43046079+hjohnson0586@users.noreply.github.com>
2023-04-20 13:57:53 -0400

    Added Explosion and Jump Effects

--- Player/Projectile.gd
+++ Player/Projectile.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 
+const ExplosionEffect = preload("res://Effects/ExplosionEffect.tscn")
+
 var velocity = Vector2.ZERO
 
 func _process(delta):
@@ -13,8 +15,10 @@ func _on_VisibilityNotifier2D_viewport_exited(viewport):
 
 
 func _on_Hitbox_body_entered(body):
+	Utils.instance_scene_on_main(ExplosionEffect, global_position)
 	queue_free()
 	
 	
 func _on_Hitbox_area_entered(area):
+	Utils.instance_scene_on_main(ExplosionEffect, global_position)
 	queue_free()
